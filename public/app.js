29025671340fb4cb4975088e3698a04823211b4a
Kent <thanhquan.kent@gmail.com>
2025-08-17 22:25:11 +0700

    Init commit
    Base project setup

---
+++ public/app.js
@@ -0,0 +1,42 @@
+"use strict";
+// fetch("/api/hello")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data.message);
+//   });
+
+class App {
+  #map;
+  #mapZoomLevel = 13;
+
+  constructor() {
+    this._getPosition();
+  }
+
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert("Could not get your position");
+        }
+      );
+    }
+  }
+
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+
+    const coords = [latitude, longitude];
+
+    this.#map = L.map("map").setView(coords, this.#mapZoomLevel);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+  }
+}
+
+const app = new App();
